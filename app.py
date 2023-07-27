5ed90bca2bed8e725b5f927ffa7e4fe56d60f62c
linh <linh.ha.nguyen20@gmail.com>
2023-07-27 16:51:14 +0700

    add wordcloud

--- app.py
+++ app.py
@@ -2,3 +2,15 @@ import streamlit as st
 
 st.title('Feature extractions from news headlines')
 
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+# Create some sample text
+text = 'Fun, fun, awesome, awesome, tubular, astounding, superb, great, amazing, amazing, amazing, amazing'
+
+# Create and generate a word cloud image
+wordcloud = WordCloud().generate(text)
+fig, ax = plt.subplots()
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+st.pyplot(fig)
