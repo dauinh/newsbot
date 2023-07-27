d9f40c3d224179088946fb3bd9a9294280386e06
linh <linh.ha.nguyen20@gmail.com>
2023-07-27 18:29:06 +0700

    generate category wordcloud

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 import streamlit as st
-
-st.title('Feature extractions from news headlines')
-
+import json
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-# Create some sample text
-text = 'Fun, fun, awesome, awesome, tubular, astounding, superb, great, amazing, amazing, amazing, amazing'
 
-# Create and generate a word cloud image
-wordcloud = WordCloud().generate(text)
-fig, ax = plt.subplots()
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
+text = 'Fun, fun, awesome, awesome, tubular, astounding, superb, great, amazing, amazing, amazing, amazing'
+def load_data():
+    processed_data = "processed.json"
+    with open(processed_data, "r") as file:
+        output = json.load(file)
+    return output
+
+def wordcloud(text):
+    # Create and generate a word cloud image
+    wordcloud = WordCloud().generate(text)
+    fig, ax = plt.subplots()
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    return fig
+
+
+data = load_data()
+text = []
+for article in data:
+    text.append(article['category'])
+res = ','.join(text)
+fig = wordcloud(res)
+
+# Streamlit app
+st.title('Feature extractions from news headlines')
 st.pyplot(fig)
