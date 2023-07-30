092da9c3c5c10047a5cb3feb750d8f5cea228cec
linh <linh.ha.nguyen20@gmail.com>
2023-07-30 19:17:29 +0700

    add user history and rec list

--- app.py
+++ app.py
@@ -16,14 +16,27 @@ def wordcloud(text):
     return fig
 
 
-articles = load_data("process.json")
+articles = load_data("processed.json")
 text = []
 for article in articles:
     text.append(article['category'])
 res = ','.join(text)
 fig = wordcloud(res)
 
+from streamlit_agraph import agraph
+from agraph import nodes, edges, config
+
+from rec import history, output
+
+for n in nodes:
+    for read in history:
+        if n.id == read:
+            n.color = "#2b5e4b"
+    for rec in output:
+        if n.id == rec:
+            n.color = "#ACDBC9"
+
 # Streamlit app
 st.title('Feature extractions from news headlines')
-from agraph import return_value
+agraph(nodes=nodes, edges=edges, config=config)
 st.pyplot(fig)
