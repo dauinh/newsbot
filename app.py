f37e86916bf7d96d94133dda8d6641d301d7b0cb
linh <linh.ha.nguyen20@gmail.com>
2023-07-31 11:14:16 +0700

    add knowledge graph

--- app.py
+++ app.py
@@ -4,13 +4,24 @@ from utils import *
 from streamlit_agraph import agraph
 from agraph import nodes, edges, config
 
-
+# Wordcloud
 articles = load_data("process.json")
 text = []
 for article in articles:
     text.append(article['category'])
 res = ','.join(text)
 fig = wordcloud(res)
+
+# Knowledge graph
+from rec import history, output
+
+for n in nodes:
+    for read in history:
+        if n.id == read:
+            n.color = "#2b5e4b"
+    for rec in output:
+        if n.id == rec:
+            n.color = "#ACDBC9"
 
 # Streamlit app
 st.title('Feature extractions from news headlines')
