a09421760a34f4dc3fa59164656fdbf40e5d02a3
linh <linh.ha.nguyen20@gmail.com>
2023-08-01 10:05:42 +0700

    add header and text

--- app.py
+++ app.py
@@ -5,7 +5,18 @@ from utils import *
 # Streamlit app
 st.title("Feature extractions from news articles")
 
+st.header("Category graph")
+st.text("Large nodes are news categories \
+        \nSmall dark blue nodes are news articles \
+        \nDark green nodes are articles that user has read \
+        \nLight green nodes are articles that we recommend")
 from category_graph import agraph
 
+st.header("Tag graph")
+st.text("Large nodes are news tags \
+        \nSmall dark blue nodes are news articles")
+from tag_graph import agraph
+
+st.header("Wordcloud from news catgories")
 fig = wordcloud()
 st.pyplot(fig)
