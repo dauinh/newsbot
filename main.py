c798e3ec54a83b9d0ae6e091ea507a0142bd5b84
linh <linh.ha.nguyen20@gmail.com>
2023-08-07 15:21:20 +0700

    add arg

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from langchain.vectorstores import FAISS
 from dotenv import load_dotenv
 
 load_dotenv()
-embeddings = OpenAIEmbeddings()
+embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
 
 @st.cache_data()
 def load_faiss_index():
@@ -30,12 +30,17 @@ keywords = st_tags(
 
 if keywords:
     parsed_preferences = '; '.join(keywords)
-    res = db.similarity_search_with_score(parsed_preferences)
+    res = db.similarity_search_with_score(
+        query=parsed_preferences,
+        k=3,
+    )
 
     st.header("Recommended articles")
     for art in res:
         doc, score = art
         st.caption(f"[{doc.metadata['title']}]({doc.metadata['url']})")
+        st.caption(f"Similarity score: {str(round(score, 3))}")
+        st.caption(doc.page_content)
 
 else:
     st.text("Please enter keywords to get recommended articles")
